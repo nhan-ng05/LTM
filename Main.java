ac8d5d10d09df435e015e4d309b6682f8f6a1b2a
nhan-ng05 <thanhnhannguyennn05@gmail.com>
2026-09-18 10:51:58 +0700

    remove all

--- Main.java
+++ Main.java
@@ -1,25 +1,6 @@
-import P2.bai5.App5;
-import P2.bai6.App6;
 
 public class Main {
     public static void main(String[] args) {
-        // App app = new App();
-        // app.ktSoNguyenTo();
-        // app.inBangCuuChuong();
 
-        // new App4();
-
-        // App10 app10 = new App10();
-        // app10.runApp();
-
-        // App8 app8 = new App8();
-        // try {
-        // app8.runApp();
-        // } catch (Exception e) {
-        // System.out.println("Co loi xay ra: " + e.getMessage());
-        // }
-
-        // App5 app5 = new App5();
-        // new App6().runApp();
     }
 }
